49c1c40cb5f7b32117aa8b7a1a86e1b5dd929bac
Sunil Sinha <sunilsinha@Nisha-Sinha.home>
2020-04-06 17:07:11 -0400

    added all the mlb team names into the word bank

---
+++ Module-2_Assessment/assets/javascript/game.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+
+var wordBank = ["braves","marlins","mets","phillies","nationals","cubs","reds","brewers","pirates","cardinals","diamondbacks","rockies","dodgers","padres","giants","orioles","red sox","yankees","rays","blue jays","white sox","indians","tigers","royals","twins","astros","angels","athletics","mariners","rangers"]
+
+
+
+})
